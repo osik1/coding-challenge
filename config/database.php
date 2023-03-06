b074074dbf410d626f5d7cb63e0485091dd9b5ab
Isaac Boampongsem <50174978+osik1@users.noreply.github.com>
2023-03-06 21:22:51 +0000

    Update database.php

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'mysql://bc80f26b3e3b00:5310cb76@us-cdbr-east-06.cleardb.net/heroku_313701d06793904?reconnect=true',
+            'host' => '',
+            'port' => '3306',
+            'database' => 'mysql://bc80f26b3e3b00:5310cb76@us-cdbr-east-06.cleardb.net/heroku_313701d06793904?reconnect=true',
+            'username' => 'bc80f26b3e3b00',
+            'password' => '5310cb76',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
